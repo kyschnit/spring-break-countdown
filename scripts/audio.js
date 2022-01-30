31943c96d27d9b031f3c9c6ed6ee15671465df17
kyschnit <kyschnit@umich.edu>
2022-01-30 16:46:14 -0500

    Initial commit

---
+++ scripts/audio.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+	var audio = document.getElementById('audio');
+	document.getElementById('vol').addEventListener("click", function () {
+		if (document.getElementById("vol").classList.contains('fa-volume-up')) {
+			audio.muted = true;
+			audio.volume = 0;
+			document.getElementById("vol").classList.remove('fa-volume-up');
+			document.getElementById("vol").classList.add('fa-volume-mute');
+		} else {
+			audio.removeAttribute('muted');
+			audio.muted = false;
+			audio.volume = 0.5;
+			audio.play();
+			document.getElementById("vol").classList.remove('fa-volume-mute');
+			document.getElementById("vol").classList.add('fa-volume-up');
+		}
+	}, false);
+})
