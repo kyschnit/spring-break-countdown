a45fe24667473f9039fb973482d9514829f26bc3
Kyle Austin Schnitzer <kyschnit@umich.edu>
2023-11-19 13:56:24 -0500

    added switching

---
+++ scripts/switch.js
@@ -0,0 +1,46 @@
+function switchToMountains() {
+	var button = document.getElementById("switch-button");
+	var video = document.getElementById("video");
+	var gif = document.getElementById("gif");
+	var source = document.getElementById("source");
+	if (video != null) {
+		video.src = "media/mountains.mp4";
+	}
+	if (gif != null) {
+		gif.src = "media/mountains.gif";
+	}
+	button.innerHTML = "Beach";
+	source.src = "media/mountainsound.mp4";
+}
+
+function switchToBeach() {
+	var button = document.getElementById("switch-button");
+	var video = document.getElementById("video");
+	var gif = document.getElementById("gif");
+	var source = document.getElementById("source");
+	if (video != null) {
+		video.src = "media/beach.mp4";
+	}
+	if (gif != null) {
+		gif.src = "media/beach.gif";
+	}
+	button.innerHTML = "Mountains";
+	source.src = "media/beachsound.mp4";
+}
+
+function switchBackground() {
+	var button = document.getElementById("switch-button");
+	if (button.innerHTML == "Beach") {
+		switchToBeach();
+	} else {
+		switchToMountains();
+	}
+}
+
+function addEventListeners() {
+	document
+		.getElementById("switch-button")
+		.addEventListener("click", switchBackground);
+}
+
+window.onload = addEventListeners;
