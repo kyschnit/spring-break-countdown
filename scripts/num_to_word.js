9b0d9dd1412eec55e8000e279ee13b759b0a1829
Kyle Schnitzer <62183867+kyschnit@users.noreply.github.com>
2024-02-22 14:50:08 -0500

    Update num_to_word.js

--- scripts/num_to_word.js
+++ scripts/num_to_word.js
@@ -51,7 +51,7 @@ function toWordsconvert(s) {
 	return str_val.replace(/\s+/g, ' ');
 }
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 24, 2024 18:00:00").getTime();
+var countDownDate = new Date("Feb 23, 2024 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
